498e66fab2f52cae01f87246eb508970c432b8c7
guilhermeforprojeto <guylhermesantos@hotmail.com>
2022-12-14 11:02:17 -0300

    Estilo ao overview

--- src/Pages/Overview/index.js
+++ src/Pages/Overview/index.js
@@ -1,7 +1,22 @@
+import overview from './overview.css'
+
 import React from 'react'
+
 
 export default function Overview() {
   return (
-    <div style={{ backgroundColor: "red" }} >  <h1>Overview</h1></div>
+    <section className='cts_overview'>  <h1>Overview</h1>
+      <section className='cts'>
+        <div className='ct_01'>
+          <span>Contas 2021</span>
+        </div>
+        <div className='ct_02'>
+          <span>Contas 2022</span>
+        </div>
+        <div className='ct_03'>
+          <span>Contas 2023</span>
+        </div>
+      </section>
+    </section>
   )
 }
