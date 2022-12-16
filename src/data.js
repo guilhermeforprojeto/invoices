829a3350f097f4bd1d09581340a03c61a185736e
guilhermeforprojeto <guylhermesantos@hotmail.com>
2022-12-16 10:31:34 -0300

    Dados do para uso com Redux

---
+++ src/data.js
@@ -0,0 +1,13 @@
+export const categories = [
+
+
+  {
+    id: 1,
+    name: "Teste",
+    movies: [
+      {
+        id: 1, title: "The boy in the asçdlkmalçksmd", released: 2008
+      }
+    ]
+  }
+]
