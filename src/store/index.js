870099930e6041159c435555b59aee2735443898
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-01-02 08:34:12 -0300

    config webtools redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore } from "redux"
 import rootReducer from "../reducers"
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__());
 
 export default store;
 
